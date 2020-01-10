e86b34a694b5ef1e9b4c6cf19bf524829ac93cd7
smartpro1 <akeni.promise@yahoo.com>
2020-01-10 22:17:45 +0100

    refactor project controller

--- src/main/java/io/akeni/promise/ppmtool/web/ProjectController.java
+++ src/main/java/io/akeni/promise/ppmtool/web/ProjectController.java
@@ -1,14 +1,19 @@
 package io.akeni.promise.ppmtool.web;
 
 import io.akeni.promise.ppmtool.model.Project;
+import io.akeni.promise.ppmtool.service.MapValidationErrorService;
 import io.akeni.promise.ppmtool.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +22,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null)
+            return errorMap;
         Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
