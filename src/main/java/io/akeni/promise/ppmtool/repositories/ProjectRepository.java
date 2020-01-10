2c7ee6dcafa281336096fa4d2a6d477c78f7e325
smartpro1 <akeni.promise@yahoo.com>
2020-01-10 07:09:08 +0100

    create repositories package and ProjectRepository

---
+++ src/main/java/io/akeni/promise/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,23 @@
+package io.akeni.promise.ppmtool.repositories;
+
+import io.akeni.promise.ppmtool.model.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    /**
+     * Returns all instances of the type {@code T} with the given IDs.
+     * <p>
+     * If some or all ids are not found, no entities are returned for these IDs.
+     * <p>
+     * Note that the order of elements in the result is not guaranteed.
+     *
+     * @param longs must not be {@literal null} nor contain any {@literal null} values.
+     * @return guaranteed to be not {@literal null}. The size can be equal or less than the number of given
+     * {@literal ids}.
+     * @throws IllegalArgumentException in case the given {@link Iterable ids} or one of its items is {@literal null}.
+     */
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+}
