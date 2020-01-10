0ce75d9b3713a1f9f2200151cf1233ae5ecc68ab
smartpro1 <akeni.promise@yahoo.com>
2020-01-10 16:24:34 +0100

    validate entity

--- src/main/java/io/akeni/promise/ppmtool/model/Project.java
+++ src/main/java/io/akeni/promise/ppmtool/model/Project.java
@@ -1,6 +1,10 @@
 package io.akeni.promise.ppmtool.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,13 +13,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message="Project name is required")
     private String projectName;
+    @NotBlank(message="Project Identifier is required")
+    @Size(min=3, max=6, message="Characters must be between 3 to 6 long")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message="Project description is required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     @PrePersist
