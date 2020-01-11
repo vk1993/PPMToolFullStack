2abcb3898e5330273500032aef8a5b836041a972
smartpro1 <akeni.promise@yahoo.com>
2020-01-11 07:59:10 +0100

    delete functionality now working

--- src/main/java/io/akeni/promise/ppmtool/service/ProjectService.java
+++ src/main/java/io/akeni/promise/ppmtool/service/ProjectService.java
@@ -21,6 +21,25 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+ "' already exists");
         }
 
+
+
+    }
+    public Project findProjectByIdentifier(String projectId){
+      Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+      if(project == null){
+          throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
+      }
+      return project;
     }
 
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null)
+            throw new ProjectIdException("Cannot find project with ID " + projectId + " This project does not exists");
+        projectRepository.delete(project);
+    }
 }
